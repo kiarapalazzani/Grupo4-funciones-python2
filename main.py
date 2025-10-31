1efbd3a8c8f4a28215c68b6f443dad68abc25e8c
Kiarapalazzani <kiaraaylenpalazzani@gmail.com>
2025-10-31 11:05:59 -0300

    Agregando archivo principal

---
+++ main.py
@@ -0,0 +1,7 @@
+# Agregando funcion principal
+# Archivo principal del grupo 
+# Cada integrante deberá agregar su función en un archivo independiente 
+from funciones import * 
+
+if __name__ == "__main__": 
+    print("Ejecución de pruebas de las funciones del grupo")
