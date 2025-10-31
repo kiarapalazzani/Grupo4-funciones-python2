ad1feee7d64c19a7a540d5b92f6bcd9e65556804
Elvira <elvira.programmer@gmail.com>
2025-10-31 14:36:14 -0300

    Agregando archivos división Fernandez

--- funciones/division_fernandezz.py
+++ funciones/division_fernandezz.py
@@ -1,5 +1,5 @@
-def dividir(a, b): 
- """Devuelve la división de dos números. Si b es 0, devuelve None.""" 
- if b == 0: 
-  return None 
-  return a / b 
+def dividir(a, b):
+    """Devuelve la división de dos números. Si el divisor es 0, devuelve None."""
+    if b == 0:
+        return None
+    return a / b
